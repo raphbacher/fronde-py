fe5b95af45f1f840215c8dafa118c503e6591169
rbacher <bacher.raphael@gmail.com>
2017-04-04 10:25:43 +0200

    add data sample

--- fronde/example.py
+++ fronde/example.py
@@ -1,7 +1,19 @@
 from fronde import fronde
 import numpy as np
+import scipy.io as sio
+
+#load data example
+Lmat = sio.loadmat('L.mat’)
+L = Lmat['L']
+
+# get robust mean and variance
+med,var = fronde(np.reshape(L, -1))
+
+print med,var
+
+# ----------------
     
-# create null samples
+# Create null samples
 x=np.random.normal(loc=1,scale=0.5,size=1000)
     
 # add outliers/alternative 
